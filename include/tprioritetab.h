2f7a7cd327a2ded5918bfcb9c46c40ef2eb70f64
elise <loui.elise@gmail.com>
2023-12-05 10:02:17 +0100

    Commit tab

--- include/tprioritetab.h
+++ include/tprioritetab.h
@@ -6,6 +6,8 @@
 
 /* -------------------------------- FUNCTIONS ------------------------------- */
 
+bool estUnTas(uint128_t *tas,int len);
+
 // Supprime le plus petit element du tas
 void supprMin(uint128_t *tas);
 
@@ -15,4 +17,7 @@ void ajout(uint128_t element, uint128_t *tas);
 // Rajoute au tas les elemennt de la liste
 void ajoutIteratif(uint128_t* listeElement, int len, uint128_t *tas);
 
+void constructionTas(uint128_t *listeElement,int len);
+
+uint128_t* unionAB(uint128_t * A,uint128_t * B,int lenA,int lenB);
 /* -------------------------------------------------------------------------- */
--- src/tprioritetab.c
+++ src/tprioritetab.c
@@ -9,6 +9,9 @@
 
 #define CAP_INIT 20
 #define MIN_ALLOC 1/4
+#define BUF_UINT128_LEN_B10 38
+#define BUF_UINT128_LEN_B16 36
+
 
 int tab_len = 0;
 int tab_capacity = CAP_INIT;
@@ -56,7 +59,7 @@ static void insertCle(uint128_t cle, uint128_t **tas) {
 
 	// Réalloue la mémoire afin de faire de la place dans le tas
 	if (tab_len == tab_capacity) {
-		*tas = realloc(*tas, 2 * tab_capacity * sizeof(uint128_t));
+		*tas = realloc(*tas, 4 * tab_capacity * sizeof(uint128_t));
 		if (tas == NULL) {
 			dprintf(STDERR_FILENO, "Erreur realloc insertCle\n");
 			exit(EXIT_FAILURE);
@@ -69,33 +72,82 @@ static void insertCle(uint128_t cle, uint128_t **tas) {
 	(*tas)[tab_len - 1] = cle;
 }
 
+// Affiche une clé à la sortie
+void print_cles(uint128_t cle) {
+	char cle_tmp[BUF_UINT128_LEN_B10] = { 0 };
+	uint128_to_str(cle, cle_tmp, BUF_UINT128_LEN_B10);
+	printf(" %s ", cle_tmp);
+}
+
+// Affiche un tableaux de clé à la sortie
+void print_tass(uint128_t *tas, int taille) {
+	printf("[\n");
+	for(int i = 0 ; i < taille; i++) {
+		print_cles(tas[i]);
+		if(i == (taille - 1)) {
+		} else {
+			printf(";\n");
+		}
+	}
+	printf("]\n");
+}
+
+// Organise un arbre/sous_arbre en tas
+static void remonteTas(uint128_t *tas,int i){
+	int k = i;
+
+	while(true){
+		int id_NodeP = k;
+		int id_ChildL = 2 * id_NodeP + 1;
+		int id_ChildR = 2 * id_NodeP + 2;
+
+		// Compare avec le fils gauche
+		if (id_ChildL < tab_len && inf(tas[id_ChildL],tas[id_NodeP])) {
+		    id_NodeP = id_ChildL;
+		}
+		// Compare avec le fils droit
+		if (id_ChildR < tab_len &&  inf(tas[id_ChildR],tas[id_NodeP])) {
+			id_NodeP = id_ChildR;
+		}
+
+		// Si le plus grand n'est pas la racine
+		if (id_NodeP != k) {
+		    interchangerAB(&tas[k],&tas[id_NodeP]);
+
+		    k = id_NodeP;
+
+		    // Récursivement effectuer le heapify sur le sous-arbre affecté
+		    //remonteTas(tas,id_NodeP);
+		}else{
+			break;
+		}
+	}
+}
+
 /* ---------------------------- PUBLIC FUNCTIONS ---------------------------- */
+bool estUnTas(uint128_t *tas,int len){
+	for (int i = len / 2 - 1; i >= 0; i--) {
+		int id_NodeP = i;
+		int id_ChildL = 2 * i + 1;
+		int id_ChildR = 2 * i + 2;
+
+		if (id_ChildL < len && inf(tas[id_ChildL],tas[id_NodeP])) {
+            return false;
+		}
+
+		if (id_ChildR < len && inf(tas[id_ChildR],tas[id_NodeP])) {
+			return false;
+	    }
+	}
+
+	return true;
+}
 
 void supprMin(uint128_t *tas) {
 	supprTete(&tas);
 
 	int i = 0;
-	int id_child = 2 * i + 1;
-	while (i < tab_len && tab_len > id_child) {
-		// Si fils gauche < racine
-		bool left_inf = inf(tas[id_child], tas[i]);
-		// Si fils droit < racine
-		bool right_inf = inf(tas[id_child + 1], tas[i]);
-
-		// Si un des fils < racine
-		if (left_inf || right_inf) {
-			if (inf(tas[id_child], tas[id_child + 1])) {
-				interchangerAB(&tas[i], &tas[id_child]);
-				i += 1 + i;
-			} else {
-				interchangerAB(&tas[i], &tas[id_child + 1]);
-				i += 2 + i;
-			}
-		} else
-			break;
-
-		id_child = 2 * i + 1;
-	}
+	remonteTas(tas,i);
 }
 
 void ajout(uint128_t cle, uint128_t *tas) {
@@ -126,6 +178,25 @@ void ajoutIteratif(uint128_t *listeElement, int len, uint128_t *tas) {
     for(int i = 0; i < len; i++) {
     	ajout(listeElement[i], tas);
     }
+}
+
+
+void constructionTas(uint128_t *listeElement,int len){
+	for (int i = len / 2 - 1; i >= 0; i--) {
+	   remonteTas(listeElement,i);
+	}
+}
+
+uint128_t * unionAB(uint128_t * A,uint128_t * B,int lenA,int lenB){
+	uint128_t * tmp = A;
+	printf("%d %d\n", lenA, lenB);
+	for(int i = 0;i<lenB;i++){
+		insertCle(B[i],&tmp);
+	}
+
+	constructionTas(tmp,lenA + lenB);
+
+	return tmp;
 }
 
 /* -------------------------------------------------------------------------- */
